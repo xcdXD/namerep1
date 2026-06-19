5134b6ddfee23cdf019d180f6c02f06f49e45f95
xcdXD <ofatnikita@gmail.com>
2026-06-19 11:58:08 +0300

    Add files via upload

---
+++ direct1/n2.py
@@ -0,0 +1,11 @@
+#region пример (регионы поставленны как метки)
+
+print("w x y z")
+for w in range(2):
+    for x in range(2):
+        for y in range(2):
+            for z in range(2):
+                f = ...             #переписываем условие
+                if f == ...:        #переписываем значение из таблицы(1 или 0)
+                    print(w, x, y, z)
+#endregion
