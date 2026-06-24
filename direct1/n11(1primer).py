c6b32b6e5bba108fb31d8d92a968bf40afd6cd3e
xcdXD <ofatnikita@gmail.com>
2026-06-25 00:10:00 +0300

    Update n11(1primer).py

--- direct1/n11(1primer).py
+++ direct1/n11(1primer).py
@@ -4,7 +4,7 @@ from math import *
 
 for bykvi in range(1,10000):
      i = ceil(log2(bykvi))
-     sernum = ceil(172 * i / 8)
+     sernum = ceil(172 * i / 8) # 172 - кол-во символов
      if 187564 * sernum <= 39 * 2 ** 20:
          print(bykvi)
           
